987acdff2fe22d11d785e8b6e77f191b2f5f4cab
Leon <yotsubato32@gmail.com>
2020-07-08 20:44:30 -0500

    Adjusting to remove sensitive info

--- scribden/src/root/global/app.ts
+++ scribden/src/root/global/app.ts
@@ -1,13 +1,8 @@
 import '@ionic/core';
-// import { setupConfig } from '../helpers/environmentConfig';
+import { setupConfig } from '../environmentConfig';
 
 export default () => {
-  /*
-    setupConfig({
-      projectURL: '',
-      projectID: '',
-      key: '',
-      secret: ''
-    })
-  */
+  setupConfig({
+    projectID: 'd7e45e90-e71c-4f6b-b62a-6d827e6c8100'
+  })
 };
